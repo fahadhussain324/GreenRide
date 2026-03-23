98819cf623f2eba6a2c779d56e0986b53db70669
Fahad Hussain <fahadhussain324@outlook.com>
2026-03-23 21:54:57 +0000

    final

--- script.js
+++ script.js
@@ -176,5 +176,64 @@
     }, 500);
   }
 
+const $ = selector => document.querySelector(selector);
+ 
+const displayErrorMsgs = msgs => {
+    const ul = document.createElement("ul");
+    ul.classList.add("messages");
+    for (let msg of msgs) {
+        const li = document.createElement("li");
+        const text = document.createTextNode(msg);
+        li.appendChild(text);
+        ul.appendChild(li);
+    }
+    const node = $(".register-section ul");
+    if (node == null) {
+        const form = $(".register-section form");
+        form.parentNode.insertBefore(ul, form);
+    } else {
+        node.parentNode.replaceChild(ul, node);
+    }
+};
+ 
+const processEntries = () => {
+    const full_name = $("#full_name");
+    const email     = $("#email_address");
+    const phone     = $("#phone");
+    const city      = $("#city");
+    const terms     = $("#terms");
+    const comments  = $("#comments");
+    const msgs = [];
+ 
+    if (full_name.value == "") { msgs[msgs.length] = "Please enter your full name.";}
+    if (email.value == "")     { msgs[msgs.length] = "Please enter an email address.";}
+    if (phone.value == "")     { msgs[msgs.length] = "Please enter a mobile phone number.";}
+    if (city.value == "")      { msgs[msgs.length] = "Please select a city.";}
+    if (terms.checked == false){ msgs[msgs.length] = "You must agree to the terms of service.";}
+    if (comments.value == "")  { msgs[msgs.length] = "Please enter a comment.";}
+ 
+    if (msgs.length == 0) {
+        $(".register-section form").submit();
+    } else {
+        displayErrorMsgs(msgs);
+    }
+};
+ 
+const resetForm = () => {
+    $(".register-section form").reset();
+    $(".register-section ul").remove();
+    $("#full_name").focus();
+};
+ 
+document.addEventListener("DOMContentLoaded", () => {
+    $("#register").addEventListener("click", processEntries);
+    $("#reset_form").addEventListener("click", resetForm);
+    $("#full_name").focus();
+});
+
+
+
   // Initialize newsletter form validation on all pages
   newsletterJoin();
+
+
